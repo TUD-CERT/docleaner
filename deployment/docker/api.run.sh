648baca03946902ac49a1db940aad35aa8f72faa
sylence <pascal.brueckner@sylence.cc>
2023-01-31 11:49:01 +0100

    Access logging disabled

--- deployment/docker/api.run.sh
+++ deployment/docker/api.run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 echo "Serving web application at ${DOCLEANER_URL}"
-uvicorn --host 0.0.0.0 --port 8080 docleaner.api.entrypoints.web.main:app
+uvicorn --host 0.0.0.0 --port 8080 --no-access-log docleaner.api.entrypoints.web.main:app
